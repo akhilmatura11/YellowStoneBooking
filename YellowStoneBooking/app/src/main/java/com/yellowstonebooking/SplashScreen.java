580edb82af9335141c6b2f8744dc8448a5e2bfae
Matura, Akhilesh <Akhilesh.Matura@fisglobal.com>
2018-11-18 20:38:20 +0530

    Yellow

---
+++ YellowStoneBooking/app/src/main/java/com/yellowstonebooking/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.yellowstonebooking;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        this.startActivity(intent);
+        finish();
+    }
+}
